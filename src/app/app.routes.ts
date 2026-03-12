61765735e52dca51cd6ef52222e06cb74fcea49b
Balltwance <danupolg@gmail.com>
2026-03-13 03:44:23 +0700

    update structure website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,25 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './modules/home/home';
+import { MainLayoutComponent } from './pages/layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+      },
+      {
+        path: 'all-students',
+        loadComponent: () =>
+          import('./pages/all-students/all-students.component').then((m) => m.AllStudentsComponent),
 
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // หน้าแรกดึงไปที่ /home
-  { path: 'home', component: HomeComponent },
-  // { path: 'menu', component: MenuComponent },
-
-  // Best Practice: ควรมีหน้า 404 เสมอกันคนพิมพ์ URL มั่ว+-
-  // { path: '**', component: NotFoundComponent }
-
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+      },
+    ],
+  },
 ];
