8843a2e5b6196b8532d2af4c1e6bd7c58bcfb8dd
Balltwance <danupolg@gmail.com>
2026-03-13 05:37:23 +0700

    update ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#F59F0A',
+      },
+    },
   },
   plugins: [],
 };
