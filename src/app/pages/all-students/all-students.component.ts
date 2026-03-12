113ff49bc272c4a129d16d83106cd5eb9ab30889
Balltwance <danupolg@gmail.com>
2026-03-13 03:49:22 +0700

    add pages

---
+++ src/app/pages/all-students/all-students.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-students',
+  imports: [],
+  templateUrl: './all-students.component.html',
+  styleUrl: './all-students.component.css',
+})
+export class AllStudentsComponent {}
