1243b0aa4bd7e669d38a420d62d35cd31467ed8d
Balltwance <danupolg@gmail.com>
2026-03-13 13:43:28 +0700

    add supabase

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core'; // ใช้ Signal
 import { RouterModule } from '@angular/router';
+import { SupabaseService } from '../../supabase';
+
+
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule], // ไม่ต้องมี CommonModule แล้ว!
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  // สร้าง Signal เพื่อเก็บรายชื่อนักศึกษา
+  students = signal<any[]>([]);
+
+  constructor(private supabase: SupabaseService) {}
+
+  async ngOnInit() {
+    const { data, error } = await this.supabase.getStudents();
+
+    if (error) {
+      console.error('พังดิครับ:', error.message);
+    } else if (data) {
+      // อัปเดตข้อมูลลงใน Signal
+      this.students.set(data);
+    }
+  }
+}
