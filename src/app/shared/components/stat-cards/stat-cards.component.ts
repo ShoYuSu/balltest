a5da682ac78addc9ba2f8c0b519f4cbb154abce5
KAY <kaolopjaiyen@gmail.com>
2026-03-22 17:35:23 +0700

    แก้หน้าstat-cards  ใช้@Input() stats: any[] = [];

--- src/app/shared/components/stat-cards/stat-cards.component.ts
+++ src/app/shared/components/stat-cards/stat-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,35 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './stat-cards.html',
 })
 export class StatCardsComponent {
-  // ตั้งค่าเป็น 0 เพราะยังไม่มีการเพิ่มผู้ใช้
-  stats = [
-    { 
-      label: 'จำนวนผู้ใช้งานทั้งหมด', 
-      value: 0, 
-      icon: 'group', 
-      bgColor: 'bg-blue-50', 
-      textColor: 'text-blue-500' 
-    },
-    { 
-      label: 'จำนวนนักศึกษา', 
-      value: 0, 
-      icon: 'school', 
-      bgColor: 'bg-green-50', 
-      textColor: 'text-green-500' 
-    },
-    { 
-      label: 'จำนวนอาจารย์', 
-      value: 0, 
-      icon: 'person', 
-      bgColor: 'bg-yellow-50', 
-      textColor: 'text-yellow-500' 
-    },
-    { 
-      label: 'ผู้ดูแลระบบ', 
-      value: 0, 
-      icon: 'assignment', 
-      bgColor: 'bg-red-50', 
-      textColor: 'text-red-500' 
-    }
-  ];
+  // รับข้อมูล Array จากหน้าไหนก็ได้ที่เรียกใช้
+  @Input() stats: any[] = []; 
 }
